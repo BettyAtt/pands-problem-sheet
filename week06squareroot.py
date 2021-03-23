9510d95dfec184cf711c6aa230139a313fc1095f
unknown <g00398300@gmit.ie>
2021-03-23 09:24:44 +0000

    week 6 task

--- week06squareroot.py
+++ week06squareroot.py
@@ -2,22 +2,20 @@
 # and creates a function which output the input squared.
 #Author: Betty Attwood
 
-def sqrt(number, number_iters = 2):
+def sqrt(number, number_iters = 5):
     a = float(number) # number program gets square root of
     # why choose a?
     for i in range(number_iters):
         #what the significance of i?
         number = 0.5 * (number + a / number)
         # Medium reference: x_(n+1) = 0.5 * (x_n +a / x_n)
-        # I guess I don't understand math
     return number
 
 n = float(input("Enter a positive number: "))
-# why n here and a above?
 if n < 0:
     print("Please enter a positive number")
     # this prevents user entering a negative number
     # can I repeat the above or loop back to it?
 else:
     answer = (sqrt(n))
-    print(round(answer,1))
+    print("The approximate square root of {} is ".format(n) + str(round(answer,1)))
